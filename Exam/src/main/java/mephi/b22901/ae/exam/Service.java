216bdd75cf4d98a1eef53347052e24c2f10c639f
Ilovemephi <egorkin.2004@mail.ru>
2025-06-04 15:37:17 +0300

    commitService
    Сделал класс Service

---
+++ Exam/src/main/java/mephi/b22901/ae/exam/Service.java
@@ -0,0 +1,81 @@
+
+package mephi.b22901.ae.exam;
+
+/**
+ *
+ * @author artyom_egorkin
+ */
+
+public class Service {
+    private int id; 
+    private String category; 
+    private String subcategory; 
+    private int price; 
+    private String requiredMechanicRole; 
+    
+
+    public Service(String category, String subcategory, int price, String requiredMechanicRole) {
+        this.id = 0; 
+        this.category = category;
+        this.subcategory = subcategory;
+        this.price = price;
+        this.requiredMechanicRole = requiredMechanicRole;
+    }
+    
+   
+    public Service(int id, String category, String subcategory, int price, String requiredMechanicRole) {
+        this.id = id;
+        this.category = category;
+        this.subcategory = subcategory;
+        this.price = price;
+        this.requiredMechanicRole = requiredMechanicRole;
+    }
+    
+
+    public int getId() {
+        return id;
+    }
+    
+    public String getCategory() {
+        return category;
+    }
+    
+    public String getSubcategory() {
+        return subcategory;
+    }
+    
+    public int getPrice() {
+        return price;
+    }
+    
+    public String getRequiredMechanicRole() {
+        return requiredMechanicRole;
+    }
+    
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    
+    public void setSubcategory(String subcategory) {
+        this.subcategory = subcategory;
+    }
+    
+    public void setPrice(int price) {
+        this.price = price;
+    }
+    
+    public void setRequiredMechanicRole(String requiredMechanicRole) {
+        this.requiredMechanicRole = requiredMechanicRole;
+    }
+    
+    @Override
+    public String toString() {
+        return "Service{id=" + id + ", category='" + category + "', subcategory='" + subcategory + 
+               "', price=" + price + ", requiredMechanicRole='" + requiredMechanicRole + "'}";
+    }
+}
