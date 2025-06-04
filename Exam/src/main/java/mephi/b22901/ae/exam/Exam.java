9f874b4214a0c942e0b0e3c7619ac36e7bd0ab11
Ilovemephi <egorkin.2004@mail.ru>
2025-06-04 13:31:04 +0300

    commit
    Начальное окно

---
+++ Exam/src/main/java/mephi/b22901/ae/exam/Exam.java
@@ -0,0 +1,14 @@
+
+
+package mephi.b22901.ae.exam;
+
+/**
+ *
+ * @author artyom_egorkin
+ */
+public class Exam {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
