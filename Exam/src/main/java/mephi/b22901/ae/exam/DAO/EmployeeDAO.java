17cff00539f7525804721c35577951957ef9fcf8
Ilovemephi <egorkin.2004@mail.ru>
2025-06-12 16:09:49 +0300

    commitEmployeeDAO
    EmployeeDAO

---
+++ Exam/src/main/java/mephi/b22901/ae/exam/DAO/EmployeeDAO.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mephi.b22901.ae.exam.DAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import mephi.b22901.ae.exam.Connection.DBConnection;
+import mephi.b22901.ae.exam.Employee;
+
+/**
+ *
+ * @author artyom_egorkin
+ */
+public class EmployeeDAO {
+    
+    public Employee getEmployeeById(int employeeId) {
+        String sql = "SELECT employee_id, full_name, role FROM Employees WHERE employee_id = ?";
+        try (
+            Connection conn = DBConnection.getConnection();
+            PreparedStatement stmt = conn.prepareStatement(sql)
+        ) {
+            stmt.setInt(1, employeeId);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                return new Employee(
+                    rs.getInt("employee_id"),
+                    rs.getString("full_name"),
+                    rs.getString("role")
+                );
+            } else {
+                return null;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Ошибка получения сотрудника по id", e);
+        }
+    }
+    
+    public List<Employee> getAllEmployees() {
+        List<Employee> employees = new ArrayList<>();
+        String sql = "SELECT employee_id, full_name, role FROM Employees";
+        try (
+            Connection conn = DBConnection.getConnection();
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery()
+        ) {
+            while (rs.next()) {
+                Employee employee = new Employee(
+                    rs.getInt("employee_id"),
+                    rs.getString("full_name"),
+                    rs.getString("role")
+                );
+                employees.add(employee);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Ошибка получения списка сотрудников", e);
+        }
+        return employees;
+    }
+    
+    
+}
