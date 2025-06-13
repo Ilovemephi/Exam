f275a6be6b96f04b31403cd3b420efb8094877d2
Ilovemephi <egorkin.2004@mail.ru>
2025-06-13 12:42:33 +0300

    update CarServiceLogic
    Метод создания новой заявки

--- Exam/src/main/java/mephi/b22901/ae/exam/Logic/CarServiceLogic.java
+++ Exam/src/main/java/mephi/b22901/ae/exam/Logic/CarServiceLogic.java
@@ -4,10 +4,52 @@
  */
 package mephi.b22901.ae.exam.Logic;
 
+import java.util.Random;
+import mephi.b22901.ae.exam.Client;
+import mephi.b22901.ae.exam.DAO.ClientDAO;
+import mephi.b22901.ae.exam.DAO.RequestDAO;
+import mephi.b22901.ae.exam.Request;
+
 /**
  *
  * @author artyom_egorkin
  */
 public class CarServiceLogic {
+    
+    private final ClientDAO clientDAO = new ClientDAO();
+    private final RequestDAO requestDAO = new RequestDAO();
+    private final Random random = new Random();
+
+    
+    public Request createNewRequest(Client client) {
+        Client dbClient = clientDAO.getClientByPhone(client.getPhoneNumber());
+        int clientId;
+        if (dbClient == null) {
+            clientId = clientDAO.addClient(client);
+        } else {
+            clientId = dbClient.getId();
+        }
+
+        boolean isService = random.nextBoolean();
+        String reason;
+        if (isService) {
+            reason = "Сервисное обслуживание";
+        } else {
+            reason = "Поломка";
+        }
+
+        String status = "Новая заявка";
+
+        Request request = new Request(clientId, reason, status);
+        int requestId = requestDAO.addRequest(request);
+        request.setId(requestId);
+
+        return request;
+    }
+    
+    
+    
+    
+    
     
 }
