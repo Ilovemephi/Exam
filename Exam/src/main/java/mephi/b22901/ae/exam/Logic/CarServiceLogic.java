4eadb50e7b099b09dfc7bd4a2782b71a79ca80d3
Ilovemephi <egorkin.2004@mail.ru>
2025-06-13 12:18:52 +0300

    commit
    начинаю работу с логикой

---
+++ Exam/src/main/java/mephi/b22901/ae/exam/Logic/CarServiceLogic.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mephi.b22901.ae.exam.Logic;
+
+/**
+ *
+ * @author artyom_egorkin
+ */
+public class CarServiceLogic {
+    
+}
